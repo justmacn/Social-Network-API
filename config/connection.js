b6085ad971645d6562f36e5331f6fc64e04e4454
justmacn <172618812+justmacn@users.noreply.github.com>
2024-08-22 22:27:13 -0400

    added server and connection js

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+// import mongoose, name db, and connect
+const { connect, connection } = require('mongoose');
+
+const connectionString = 'mongodb://127.0.0.1:27017/socialNetworkDB';
+
+connect(connectionString);
+
+module.exports = connection;
+
